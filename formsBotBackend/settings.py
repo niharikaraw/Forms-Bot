89620a9ac24a621b006fbbb95ee4b1b76bf081e1
niharikaraw <84120092+niharikaraw@users.noreply.github.com>
2022-08-01 15:15:37 +0530

    Update settings.py

--- formsBotBackend/settings.py
+++ formsBotBackend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-88b!5@(m6oh(!yx2pz2mkvocvg4sunz_@u=h0=gzl#ac!vi#k*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
